6f55a0b2addd83a17d8cb6dc23ae6497933a65d2
Raúl Chirinos <raulstuf@gmail.com>
2020-07-02 19:46:11 -0400

    Create and implement page container

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 
-import NewClient from './pages/NewClient';
+import Navbar from '././components/Navbar';
+import Clients from '././pages/Clients';
+import PageContainer from './components/PageContainer';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NewClient />
+      <Navbar />
+      <PageContainer>
+        <Clients />
+      </PageContainer>
     </ThemeProvider>
   );
 }
