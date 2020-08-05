d0379b666955fd1724d99efdd012996b99ac38db
Raúl Chirinos <raulstuf@gmail.com>
2020-08-05 00:14:07 -0400

    Create SideBar Menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 
-import Navbar from '././components/Navbar';
+// import Navbar from '././components/Navbar';
 import Clients from './pages/Clients/Clients';
+import SideMenu from './components/SideMenu/SideMenu';
 import PageContainer from './components/PageContainer/PageContainer';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
+      {/* <Navbar /> */}
+      <SideMenu />
       <PageContainer>
         <Clients />
       </PageContainer>
---
+++ client/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faColumns,
+  faCommentDollar,
+  faChild,
+  faBox,
+  faBarcode
+} from '@fortawesome/free-solid-svg-icons';
+import './SideMenu.scss';
+
+let pages = [
+  {
+    id: 1,
+    name: 'Dashboard',
+    icon: faColumns,
+    route: '/dashboard'
+  },
+  {
+    id: 2,
+    name: 'Sales',
+    icon: faCommentDollar,
+    route: '/sales'
+  },
+  {
+    id: 3,
+    name: 'Clients',
+    icon: faChild,
+    route: '/clients'
+  },
+  {
+    id: 4,
+    name: 'Providers',
+    icon: faBox,
+    route: '/providers'
+  },
+  {
+    id: 5,
+    name: 'Products',
+    icon: faBarcode,
+    route: 'products'
+  }
+];
+
+export default function SideMenu() {
+  const [activePage, setActivePage] = useState(1);
+
+  return (
+    <>
+      <div className="side-menu">
+        <div className="side-menu_menu">
+          {pages.map((page) => (
+            <span
+              onClick={() => setActivePage(page.id)}
+              className={`side-menu_item ${page.id === activePage ? 'active' : ''}`}
+            >
+              <FontAwesomeIcon className="side-menu_item-icon" icon={page.icon} />
+              {page.name}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
